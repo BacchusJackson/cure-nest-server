f00ae6bd67dcb02b8d477d772f5c1a6d567dcacc
bacchusjackson <bjjackson32@gmail.com>
2019-07-20 15:16:34 -0400

    added logging

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Param, Post, Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, Logger } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  private logger = new Logger('**UsersController');
   constructor(private readonly usersService: UsersService) { }
 
   @Post()
   postUserRequest(@Body() newUser) {
+    this.logger.log('New User Request, Username: ' + newUser.username);
     return this.usersService.createUser(newUser);
   }
 
@@ -32,6 +34,7 @@ export class UsersController {
 
   @Delete('/id/:id')
   deleteUserRequest(@Param('id') id) {
+    this.logger.log('Delete User Request, id:' + id)
     return this.usersService.deleteUser(id);
   }
 }
